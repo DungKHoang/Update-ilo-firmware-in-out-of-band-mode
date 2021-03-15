470f3e48c65d6b085291c983937ca770c46bd491
Dung K Hoang <DungKHoang@gmail.com>
2021-03-15 09:03:12 -0700

    v1.0
    Signed-off-by: Dung K Hoang <DungKHoang@gmail.com>

---
+++ HPOV-update-ilo-firmware.ps1
@@ -0,0 +1,55 @@
+# ------------------ Parameters
+Param (                    
+        [string]$hostName                  = "", 
+        [string]$userName                  = "", 
+        [string]$password                  = "",
+        [string]$authLoginDomain           = "local",
+        [string]$minFWversion              = '2.00',
+        [string]$iloFWlocation             = "C:\Users\admin\Desktop\Firmware\ilo5.bin"
+      )
+
+
+
+
+
+if ($hostName -or $userName -or $password)
+    ### Connect to OneView
+    $securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+    $cred           = New-Object System.Management.Automation.PSCredential  -ArgumentList $userName, $securePassword
+
+
+    write-host -ForegroundColor Cyan "---- Connecting to OneView --> $hostName"
+    $OVconnection   = Connect-HPOVMgmt -Hostname $hostName -loginAcknowledge:$true -AuthLoginDomain $authLoginDomain -Credential $cred
+
+
+
+    # ----------------- Get list of servers whose iLO FW  less than 2.00
+    $serverList                 = Get-HPOVserver | where mpFirmwareVersion -le $minFWversion
+    if ($serverList)
+    {
+        foreach ($s in $serverList)
+        {
+            $serverName         = $s.Name 
+            $iloSession         = $s | Get-HPOViloSso -IloRestSession
+            $authToken          = $iloSession.'X-Auth-Token'
+            $iloIP              = $s.mpHostInfo.mpIpAddresses[0].address
+            
+
+            write-host -foregraound CYAN "Updating iLO firmware of server $serverName...."
+            #### Connect to iLO
+            # Using AuthToken works ONLY for iLO5
+
+            $iLOConnection      = Connect-HPEiLO -Address $iloIP -XAuthToken $authToken -DisableCertificateAuthentication
+            Update-HPEiLOFirmware -Connection $iLOConnection -Location $iloFWlocation  -UploadTimeout 700
+            
+        }
+    }
+    else
+    {
+        write-host -foreground YELLOW 'No server with iLO firmware lower than 2.00. Spkip updating iLO FW.....'
+    }
+}
+else
+{
+    No OneView IP nor userName nor password specified. Exit the script
+}
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Update-ilo-firmware-in-out-of-band-mode
+# Upodate iLO fiwrmware in Out-of-band mode
+  The script enables you to update iLO firmware of servers in iLO where the iLO firmware version is lower than 2.00
+
+## Notes
+   * Access iLO from OneView
+   * Works only for **iLO5 with Authentcation token key**. 
+
+
+## How to get Support
+Simple scripts or tools posted on github are provided AS-IS and support is absed on best effort provided by the author. If you encunter problems with the script, please submit an issue 
+
+## Prerequisites
+The script requires:
+   * the latest OneView PowerShell library on PowerShell gallery
+   * the latest HPEiLOCmdelts on PowerShell gallery
+   * Download the latest iLO firmware to a local folder
+
+  
+
+## To install OneView PowerShell library and HPEiLOCmdlets
+
+```
+    install-module HPEOneView.5xx  -scope currentuser
+    install-Module HPEiLOcmdlets   -scope currentuser
+    
+
+```
+
+## To run in an OneView environment
+
+```
+    # ONLY for iLO 5
+    .\update-iLO-firmware.ps1 -hostname <OV-name> -username <OV-admin> -password <OV-password> -iloFWlocation c:\ilo5.bin -minFWversion '2.00'
+
+```
+
+    
